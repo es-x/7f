de926832d010061c71cba8aef728788670418d06
Олег Владимирович Евдокимов <git@esx.su>
2025-09-27 18:13:15 +0300

    func TestCafeCount

--- main_test.go
+++ main_test.go
@@ -3,10 +3,12 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeNegative(t *testing.T) {
@@ -46,5 +48,39 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	for k := range cafeList {
+		requests := []struct {
+			count int // передаваемое значение count
+			want  int // ожидаемое количество кафе в ответе
+		}{
+			{count: 0, want: 0},
+			{count: 1, want: 1},
+			{count: 2, want: 2},
+			{count: 100, want: min(len(cafeList[k]), 100)},
+		}
+
+		for _, v := range requests {
+			response := httptest.NewRecorder()
+			count := strconv.Itoa(v.count)
+
+			req := httptest.NewRequest("GET", "/cafe?city="+k+"&count="+count, nil)
+			handler.ServeHTTP(response, req)
+
+			require.Equal(t, http.StatusOK, response.Code)
+			//fmt.Println(strings.Split(response.Body.String(), ","), len(strings.Split(response.Body.String(), ",")))
+			str := strings.Split(response.Body.String(), ",")
+			if str[0] != "" {
+				assert.Len(t, str, v.want)
+			} else {
+				assert.Len(t, str, v.want+1)
+			}
+		}
+
 	}
 }
